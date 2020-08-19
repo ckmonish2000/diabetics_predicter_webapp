2abc04e214a67b17ea8d449eaf12e845d56aac99
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-19 19:16:31 +0530

    convert to pytorch tensor

--- model/model.py
+++ model/model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 data=pd.read_csv("diabetes2.csv")
@@ -16,8 +17,24 @@ features=["Pregnancies","Glucose","BloodPressure","SkinThickness","Insulin","BMI
 y=data.pop("Outcome")
 X=data[features]
 
-# 
-print("features: \n")
-print(f"{X.head(5)}")
-print("labels : \n")
-print(f"{y.head(5)}")
+# display of data features and labels
+
+# print("features: \n")
+# print(f"{X.head(5)}")
+# print("labels : \n")
+# print(f"{y.head(5)}")
+
+
+X=np.array(X)
+y=np.array(y)
+# train test split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1234)
+
+# convert to numpy tensors
+X_train=torch.from_numpy(X_train)
+y_train=torch.from_numpy(y_train)
+X_test=torch.from_numpy(X_test)
+y_test=torch.from_numpy(y_test)
+
+
