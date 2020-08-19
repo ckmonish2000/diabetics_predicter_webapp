d39f572fb7e229c8d41a36a2940f6913fa507c57
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-19 21:41:24 +0530

    minor server init

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 
 
 app=Flask(__name__)
 
-
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
